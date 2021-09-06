c3b2e366da65fd7b16267d1035d79fb64a3a290e
Ajay <ajayhegde676@gmail.com>
2021-09-06 23:23:11 +0530

    remove unnecessary logs

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-// console.log(('this ia log from js file'));
-// fetch('http://localhost:3000/weather?address=sirsi').then(response =>{
-//     response.json().then(data =>{
-//         if(data.error){
-//             console.log(data.error);
-//         }
-//         else{
-//             console.log(data);
-//         }
-//     })
-// })
 
 const weatherForm=document.querySelector('form')
 const search=document.querySelector('input')
